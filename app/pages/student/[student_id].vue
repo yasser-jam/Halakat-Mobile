9dedd2bb7d45ffd228db486742b1c93dd38690b0
yasserjamalaldeen <yasserjamalaldeen@gmail.com>
2024-12-07 22:16:52 +0300

    student page layout

--- app/pages/student/[student_id].vue
+++ app/pages/student/[student_id].vue
@@ -2,32 +2,23 @@
   <div class="mx-auto p-8 container">
     <div class="mb-10 flex flex-col items-center justify-center">
       <img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg" width="100" class="mb-2 rounded-full">
-      <div>اسم الطالب</div>
+      <div class="text-gray-700 font-semibold">
+        اسم الطالب
+      </div>
     </div>
 
     <div class="text-xs font-semibold">
-      معلومات الأهل
+      معلومات الأب
     </div>
 
-    <div class="grid grid-cols-2 mb-12">
+    <div class="grid mb-12">
       <div class="flex items-center justify-between">
         <div class="mt-2 flex items-center gap-2">
           <div class="text-xs text-gray-500">
             اسم الأب
           </div>
           <div class="badge badge-primary">
-            اسم الأب
-          </div>
-        </div>
-      </div>
-
-      <div class="flex items-center justify-between">
-        <div class="mt-2 flex items-center gap-2">
-          <div class="text-xs text-gray-500">
-            اسم الأم
-          </div>
-          <div class="badge badge-primary">
-            اسم الأب
+            عباس العقاد
           </div>
         </div>
       </div>
@@ -38,18 +29,7 @@
             حالة الأب
           </div>
           <div class="badge badge-primary">
-            اسم الأب
-          </div>
-        </div>
-      </div>
-
-      <div class="flex items-center justify-between">
-        <div class="mt-2 flex items-center gap-2">
-          <div class="text-xs text-gray-500">
-            حالة الأم
-          </div>
-          <div class="badge badge-primary">
-            اسم الأب
+            موجود
           </div>
         </div>
       </div>
@@ -60,18 +40,7 @@
             رقم هاتف الأب
           </div>
           <div class="badge badge-primary">
-            اسم الأب
-          </div>
-        </div>
-      </div>
-
-      <div class="flex items-center justify-between">
-        <div class="mt-2 flex items-center gap-2">
-          <div class="text-xs text-gray-500">
-            رقم هاتف الأم
-          </div>
-          <div class="badge badge-primary">
-            اسم الأب
+            099344166
           </div>
         </div>
       </div>
@@ -86,14 +55,64 @@
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="text-xs font-semibold">
+      معلومات الأم
+    </div>
+
+    <div class="grid mb-12">
+      <div class="flex items-center justify-between">
+        <div class="mt-2 flex items-center gap-2">
+          <div class="text-xs text-gray-500">
+            اسم الأم
+          </div>
+          <div class="badge badge-error">
+            هنادي الملك
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="mt-2 flex items-center gap-2">
+          <div class="text-xs text-gray-500">
+            حالة الأم
+          </div>
+          <div class="badge badge-error">
+            موجود
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="mt-2 flex items-center gap-2">
+          <div class="text-xs text-gray-500">
+            رقم هاتف الأم
+          </div>
+          <div class="badge badge-error">
+            0945644687
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="mt-2 flex items-center gap-2">
+          <div class="text-xs text-gray-500">
+            عمل الأم
+          </div>
+          <div class="badge badge-error">
+            مدرسة
+          </div>
+        </div>
+      </div>
 
       <div class="flex items-center justify-between">
         <div class="mt-2 flex items-center gap-2">
           <div class="text-xs text-gray-500">
             حالة الزواج
           </div>
-          <div class="badge badge-primary">
-            اسم الأب
+          <div class="badge badge-error">
+            متزوجان
           </div>
         </div>
       </div>
